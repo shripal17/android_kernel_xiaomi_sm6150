b59f7c704d27f8dd6361031eba8aaaa3a5822648
Pzqqt <821026875@qq.com>
2021-07-13 19:13:28 +0800

    build.sh: Rewrite

--- build.sh
+++ build.sh
@@ -1,77 +1,50 @@
 #!/bin/bash
 
-kernel_dir="${PWD}"
-CCACHE=$(command -v ccache)
-HOME=/home/rasenkai
-objdir="${kernel_dir}/out"
-anykernel=$HOME/kernel/asus/anykernel
-builddir="${kernel_dir}/build"
-ZIMAGE=$kernel_dir/out/arch/arm64/boot/Image.gz
-kernel_name="Requiem-Nightly"
-zip_name="$kernel_name-$(date +"%d%m%Y").zip"
-GCC_DIR=$HOME/kernel/toolchain/gcc9
-export CONFIG_FILE="phoenix_defconfig"
-export ARCH="arm64"
-export KBUILD_BUILD_HOST="TheWorld"
-export KBUILD_BUILD_USER="BuriBuriZaemon"
+yellow='\033[0;33m'
+white='\033[0m'
+red='\033[0;31m'
+gre='\e[0;32m'
+ZIMG=./out/arch/arm64/boot/Image.gz-dtb
+OUTPUT_DIR=./../Paradox_release
 
-PATH=/home/rasenkai/kernel/toolchain/aospclang/bin/:$PATH
+export LOCALVERSION=-test
 
-# Colors
-NC='\033[0m'
-RED='\033[0;31m'
-LRD='\033[1;31m'
-LGR='\033[1;32m'
+rm -f $ZIMG
 
-make_defconfig()
-{
-	# Needed to make sure we get dtb built and added to kernel image properly
-     START=$(date +"%s")
-	echo -e ${LGR} "############### Cleaning ################${NC}"
-    rm $anykernel/Image.gz
-    rm -rf $ZIMAGE
+export ARCH=arm64
+export SUBARCH=arm64
+export HEADER_ARCH=arm64
+export CLANG_PATH=/home/pzqqt/build_toolchain/clang-r416183d-12.0.7
 
-	echo -e ${LGR} "########### Generating Defconfig ############${NC}"
-    make -s ARCH=${ARCH} O=${objdir} ${CONFIG_FILE} -j$(nproc --all)
-}
-compile()
-{
-	cd ${kernel_dir}
-	echo -e ${LGR} "######### Compiling kernel #########${NC}"
-	make -j$(nproc --all) O=out \
-                      ARCH=${ARCH}\
-                      CC="ccache clang" \
-	                CLANG_TRIPLE="aarch64-linux-gnu-" \
-	                CROSS_COMPILE="aarch64-linux-gnu-" \
-	                CROSS_COMPILE_ARM32="arm-linux-gnueabi-" \
-	                -j4
-}
+export KBUILD_BUILD_HOST="wsl2"
+export KBUILD_BUILD_USER="pzqqt"
 
-completion() 
-{
-	cd ${objdir}
-	COMPILED_IMAGE=arch/arm64/boot/Image.gz
-	COMPILED_DTBO=arch/arm64/boot/dtbo.img
-	if [[ -f ${COMPILED_IMAGE} && ${COMPILED_DTBO} ]]; then
-		mv -f $ZIMAGE ${COMPILED_DTBO} $anykernel
-        cd $anykernel
-        find . -name "*.zip" -type f
-        find . -name "*.zip" -type f -delete
-        zip -r AnyKernel.zip *
-        mv AnyKernel.zip $zip_name
-        mv $anykernel/$zip_name $HOME/Desktop/$zip_name
-        END=$(date +"%s")
-        DIFF=$(($END - $START))
-		echo -e ${LGR} "############################################"
-		echo -e ${LGR} "############# OkThisIsEpic!  ##############"
-		echo -e ${LGR} "############################################${NC}"
-	else
-		echo -e ${RED} "############################################"
-		echo -e ${RED} "##         This Is Not Epic :'(           ##"
-		echo -e ${RED} "############################################${NC}"
-	fi
-}
-make_defconfig
-compile
-completion
-cd ${kernel_dir}
+ccache_=`which ccache`
+
+make mrproper O=out || exit 1
+make phoenix_defconfig O=out || exit 1
+
+Start=$(date +"%s")
+
+make -j$(nproc --all) \
+	O=out \
+	CC="${ccache_} ${CLANG_PATH}/bin/clang" \
+	CLANG_TRIPLE=aarch64-linux-gnu- \
+	CROSS_COMPILE="/home/pzqqt/build_toolchain/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-" \
+	CROSS_COMPILE_ARM32="/home/pzqqt/build_toolchain/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-"
+
+exit_code=$?
+End=$(date +"%s")
+Diff=$(($End - $Start))
+
+if [ -f $ZIMG ]; then
+	mkdir -p $OUTPUT_DIR
+	cp -f ./out/arch/arm64/boot/Image.gz $OUTPUT_DIR/Image.gz
+	cp -f ./out/arch/arm64/boot/dts/qcom/sdmmagpie.dtb $OUTPUT_DIR/dtb
+	cp -f ./out/arch/arm64/boot/dtbo.img $OUTPUT_DIR/dtbo.img
+	which avbtool &>/dev/null && python2 `which avbtool` add_hash_footer --partition_name dtbo --partition_size $((32 * 1024 * 1024)) --image $OUTPUT_DIR/dtbo.img
+	echo -e "$gre << Build completed in $(($Diff / 60)) minutes and $(($Diff % 60)) seconds >> \n $white"
+else
+	echo -e "$red << Failed to compile Image.gz-dtb, fix the errors first >>$white"
+	exit $exit_code
+fi
