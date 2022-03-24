394bb3ef639b6218182eb5a521301bcdab2e7097
Pzqqt <821026875@qq.com>
2022-03-24 17:03:10 +0800

    asoc: codecs: Update tfa98xx driver to v6.6.3
    Follow https://github.com/sm6150-dev/android_kernel_xiaomi_sm6150/commits/221229d4e4b4f37fb5b55f292a7cc221e7aebc4f/techpack/audio/asoc/codecs/tfa98xx
    
    Also replace all Windows format newlines to Unix format.
    
    Signed-off-by: Pzqqt <821026875@qq.com>

--- techpack/audio/asoc/codecs/tfa98xx/inc/tfa_device.h
+++ techpack/audio/asoc/codecs/tfa98xx/inc/tfa_device.h
@@ -1,20 +1,11 @@
 /*
- * Copyright 2014-2017 NXP Semiconductors
- * Copyright (C) 2021 XiaoMi, Inc.
+ * Copyright (C) 2014 NXP Semiconductors, All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
-
 
 /**\file
  *
@@ -186,9 +177,7 @@ int tfa_dev_probe(int slave, struct tfa_device *tfa);
  *  @param vstep the selected vstep to use
  *  @return tfa_error enum
  */
-
 enum tfa_error tfa_dev_start(struct tfa_device *tfa, int next_profile, int vstep, u8 pcm_format);
-
 
 /**
  * Stop audio for this instance as gracefully as possible.
@@ -222,7 +211,7 @@ enum tfa_error tfa_dev_stop(struct tfa_device *tfa);
  *  @param state struct = desired device state after function return
  *  @return tfa_error enum
  */
-enum tfa_error tfa_dev_set_state(struct tfa_device *tfa, enum tfa_state state, int is_calibration);
+enum tfa_error tfa_dev_set_state(struct tfa_device *tfa, enum tfa_state state,int is_calibration);
 
 /**
  * Retrieve the current state of this instance in an active way.
@@ -295,3 +284,4 @@ int tfa_irq_unmask(struct tfa_device *tfa);
 //debug?
 
 #endif /* __TFA_DEVICE_H__ */
+
