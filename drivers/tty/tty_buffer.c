15d30d2e00a4e715b90b6ab19940eb3de89c32bf
Pzqqt <821026875@qq.com>
2022-07-30 18:32:01 +0800

    tty: Reapply patch 63f11da2f99d12fe1330d651881b2d0709ff8248
    Signed-off-by: Pzqqt <821026875@qq.com>

--- drivers/tty/tty_buffer.c
+++ drivers/tty/tty_buffer.c
@@ -544,7 +544,7 @@ void tty_flip_buffer_push(struct tty_port *port)
 	struct tty_bufhead *buf = &port->buf;
 
 	tty_flip_buffer_commit(buf->tail);
-	queue_work(system_unbound_wq, &buf->work);
+	kthread_queue_work(&port->worker, &buf->work);
 }
 EXPORT_SYMBOL(tty_flip_buffer_push);
 
@@ -574,7 +574,7 @@ int tty_insert_flip_string_and_push_buffer(struct tty_port *port,
 		tty_flip_buffer_commit(buf->tail);
 	spin_unlock_irqrestore(&port->lock, flags);
 
-	queue_work(system_unbound_wq, &buf->work);
+	kthread_queue_work(&port->worker, &buf->work);
 
 	return size;
 }
