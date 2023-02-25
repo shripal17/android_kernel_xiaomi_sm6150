e5f2a2d7172090dcd86a30a6b5bb68c845b40fb6
Pzqqt <821026875@qq.com>
2023-02-25 19:58:09 +0800

    build.sh: v5.9

--- build.sh
+++ build.sh
@@ -47,7 +47,7 @@ export ARCH=arm64
 export KBUILD_BUILD_HOST="wsl2"
 export KBUILD_BUILD_USER="pzqqt"
 
-export LOCALVERSION=-v5.8
+export LOCALVERSION=-v5.9
 
 ccache_=
 (! $no_ccache) && ccache_=`which ccache` || echo -e "${yellow}Warning: ccache is not used! $white"
