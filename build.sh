164038e9548d7eed807f8c20d195e807406c471e
sajadasadollahi83 <sajadasadollahi84@gmail.com>
2024-05-13 17:19:10 +0330

    Update Build Script
    This commit introduces a new build script for building the project with Clang 18

--- build.sh
+++ build.sh
@@ -1,13 +1,26 @@
+export CLANG_PATH=/home/sajad/toolchains/clang-r510928
+export PATH=${CLANG_PATH}/bin:${PATH}
+
 export ARCH=arm64
 export KBUILD_BUILD_USER=ventur
-export PATH="$(pwd)/toolchain"/bin/:$PATH
+export KBUILD_BUILD_HOST=Sajad-PC
 
-make O=out ARCH=arm64 phoenix_defconfig
-make -j$(nproc) \
+# Clean the build directory
+make clean O=out ARCH=arm64
+
+# Remove any configuration or generated files 
+make mrproper O=out ARCH=arm64
+
+# Load the configuration file
+make phoenix_defconfig O=out ARCH=arm64
+
+# Build the kernel
+make -j$(nproc --all) \
     O=out \
     ARCH=arm64 \
     LLVM_IAS=1 \
     CC="ccache clang" \
     LD=ld.lld \
- CROSS_COMPILE=aarch64-linux-gnu- \
- CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+    CROSS_COMPILE="/home/sajad/toolchains/arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-" \
+    CROSS_COMPILE_ARM32="/home/sajad/toolchains/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-"
+
